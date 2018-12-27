968fcc538f0435b22821527c482edcae7dd2a2da
a1an1in <a1an1in@sina.com>
2018-12-27 12:52:04 +0800

    [del:player-android] not suitable to put this module in this lib

--- src/apps/player-android/app/src/main/jni/native-lib.c
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-
-JNIEXPORT jstring JNICALL Java_org_libsdl_app_SDLActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject  this ) {
-
-    return (*env)->NewStringUTF(env, "hello world");
-}
