ac291f8ecca1dda84a0cb5f3a03283782333468a
a1an1in <a1an1in@sina.com>
2019-07-19 16:04:36 +0800

    prepare change include file names

--- src/include/libobject/core/utils/alloc/allocator_sys_alloc.h
+++
@@ -1,4 +0,0 @@
-#ifndef __ALLOCATOR_SYSALLOC_H__
-#define __ALLOCATOR_SYSALLOC_H__
-int allocator_sys_alloc_register();
-#endif
--- src/include/libobject/core/utils/dbg/inc_files.h
+++
@@ -1,8 +0,0 @@
-#ifndef __INC_FILES_H__
-#define __INC_FILES_H__
-
-#include "debugger/basic_types.h"
-#include "debugger/debug.h"
-#include "debugger/list.h"
-
-#endif
