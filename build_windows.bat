dd40f9275d9579f6ad36c7de9cf7d00d93c57cd4
alan <a1an1in@sina.com>
2020-09-12 13:29:56 +0800

    Update build_windows.bat

--- build_windows.bat
+++ build_windows.bat
@@ -1,4 +1,4 @@
-
-mkdir -p build/windows
-cd build/windows
-cmake.exe ../.. -DPLATFORM=windows && mingw32-make.exe
+mkdir -p build\windows
+cd /d build\windows
+cmake.exe -S ..\.. -DPLATFORM=windows  -G "MinGW Makefiles" -DCMAKE_MAKE=mingw32-make
+mingw32-make.exe
