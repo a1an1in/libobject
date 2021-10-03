f612b1f9c76908344af38fc311023ccf65e27a42
alan <a1an1in@sina.com>
2021-10-03 16:41:38 +0800

    move Obj.c policy to policy.c

--- src/include/libobject/core/policy.h
+++ src/include/libobject/core/policy.h
@@ -7,6 +7,7 @@
 #include <libobject/core/utils/registry/registry.h>
 #include <libobject/core/Vector.h>
 #include <libobject/core/String.h>
+#include <libobject/core/Number.h>
 #include <libobject/core/try.h>
 #include <libobject/argument/Command.h>
 
@@ -14,5 +15,15 @@ typedef struct vector_to_json_policy_s {
     int (*policy)(cjson_t *root, void *element);
 } vector_to_json_policy_t;
 
+typedef struct obj_set_policy_s {
+    int (*policy)(Obj *obj, class_info_entry_t *entry, void *value);
+} obj_set_policy_t;
+
+typedef struct obj_to_json_policy_s {
+    int (*policy)(cjson_t *json, char *name, void *value);
+} obj_to_json_policy_t;
+
+extern obj_to_json_policy_t g_obj_to_json_policy[ENTRY_TYPE_MAX_TYPE];
+extern obj_set_policy_t g_obj_set_policy[ENTRY_TYPE_MAX_TYPE];
 extern vector_to_json_policy_t g_vector_to_json_policy[ENTRY_TYPE_MAX_TYPE];
 #endif
