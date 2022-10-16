e471bdc0f09d5e7aedba2f9983651482750b944a
alan <a1an1in@sina.com>
2022-10-16 16:52:41 +0800

    Mv test into project root dir
    Description:
    Do little changes
    
    Major Changes:
    1. Change test dir
    2. Change cmake file

--- build_mac.sh
+++ build_mac.sh
@@ -5,4 +5,4 @@ cd build/mac
 cmake ../.. -DPLATFORM=mac &&make
 #make install
 cd ../..
-printf '\x07' | dd of=sysroot/mac/bin/main bs=1 seek=160 count=1 conv=notrunc
+printf '\x07' | dd of=sysroot/mac/bin/xtools bs=1 seek=160 count=1 conv=notrunc
